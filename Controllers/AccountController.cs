9e06b10df0b3d226e23557fca54bd788dcbebbd9
hosamff2001 <hosamff2001@gmail.com>
2024-02-09 18:12:56 +0200

    init app

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+
+public class AccountController : Controller
+{
+    public ActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Login(LoginViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            // Implement your authentication logic here, e.g., check username and password against database
+            if (model.UserName == "admin" && model.Password == "password")
+            {
+                // If authentication succeeds, redirect to home page or any desired page
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid username or password.");
+            }
+        }
+
+        // If authentication fails, return the login view with error message
+        return View(model);
+    }
+}
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class LoginViewModel
+{
+    [Required]
+    public string UserName { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+}
